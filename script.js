b0946c43aee7265bb587df774aec93099a203faf
shlokkyyyy <shlokkhandelwalop@gmail.com>
2025-07-22 09:26:38 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-
-function checkLogin() {
-    var user = document.getElementById("username").value;
-    var pass = document.getElementById("password").value;
-    if(user === "tanyaa777_" && pass === "230707") {
-        document.getElementById("login").style.display = "none";
-        document.getElementById("main-content").style.display = "block";
-    } else {
-        document.getElementById("login-msg").innerText = "Incorrect username or password.";
-    }
-}
+// JavaScript for animations
+console.log('Birthday site loaded');
